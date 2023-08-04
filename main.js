427a8fb28d115fcb5083d54a47dbf2f78bdd5fdd
ADENIYI Oluwafemi Opeyemi <132268090+ojoyeuge1@users.noreply.github.com>
2023-08-04 08:50:26 +0100

    my second commit: update design, style and function

--- main.js
+++ main.js
@@ -1,27 +1,61 @@
-function submit(){
-    
-    document.getElementById("dis1").innerHTML = "NGN " + eval(Number(document.getElementById("inp1").value) + Number(document.getElementById("inp2").value) + Number(document.getElementById("inp3").value) + Number(document.getElementById("inp4").value))
-    document.getElementById("dis2").innerHTML = "NGN " + 0
-    document.getElementById("inp1").value = " "
-    document.getElementById("inp2").value = " "
-    document.getElementById("inp3").value = " "
-    document.getElementById("inp4").value = " "
-    // var form = document.getElementById("frm")
-    // form.style.display = "none"
-    // var formExp = document.getElementById("frm-exp")
-    // formExp.style.display = "block"
+
+
+let form = document.getElementById("frm");
+let formExp = document.getElementById("frm-exp");
+let sub = document.getElementById("sub");
+let clear = document.getElementById("rst");
+let exp = document.getElementById("nxt");
+let details = document.createElement("list");
+details.setAttribute("id", "dis3-body");
+let dvBtn = document.createElement("div")
+let btnEdit = document.createElement("button")
+let btnDel = document.createElement("button")
+dvBtn.append(btnEdit)
+dvBtn.appendChild(btnDel)
+
+
+
+
+const submit=()=>{
+    let totalIncome = eval(Number(document.getElementById("inp1").value) + Number(document.getElementById("inp2").value) + Number(document.getElementById("inp3").value) + Number(document.getElementById("inp4").value))
+    document.getElementById("dis1").innerHTML = "NGN " + totalIncome;
+    document.getElementById("dis2").innerHTML = "NGN " + 0;
+    sub.style.display = "none";    
+    exp.style.display = "block"
        
 }
 
-function clr(){
+const clr=()=>{
 
-    document.getElementById("inp1").value = " "
-    document.getElementById("inp2").value = " "
-    document.getElementById("inp3").value = " "
-    document.getElementById("inp4").value = " "
-    // document.getElementById("dis1").innerHTML = " "
-    // document.getElementById("dis2").innerHTML = " "
+    document.getElementById("inp1").value = " ";
+    document.getElementById("inp2").value = " ";
+    document.getElementById("inp3").value = " ";
+    document.getElementById("inp4").value = " ";
+    document.getElementById("dis1").innerHTML = " ";
+    document.getElementById("dis2").innerHTML = " ";
+    sub.style.display = "block";    
+    exp.style.display = "none"
      
+}
+
+const next=()=>{
+
+    form.style.display = "none";
+    formExp.style.display = "block"
+}
+
+const subt=()=>{
+    let totalIncome = eval(Number(document.getElementById("inp1").value) + Number(document.getElementById("inp2").value) + Number(document.getElementById("inp3").value) + Number(document.getElementById("inp4").value))
+    let expense = Number(document.getElementById("inp6").value)
+    document.getElementById("dis2").innerHTML = "NGN " + eval(totalIncome - expense)
+}
+
+const rest=()=>{
+    document.getElementById("dis1").innerHTML = " ";
+    document.getElementById("dis2").innerHTML = " ";
+    form.style.display = "block";
+    formExp.style.display = "none"
+
 }
 
 // var disHead = document.createElement("div");
